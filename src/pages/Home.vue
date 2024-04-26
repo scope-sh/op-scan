7ace16e13f66784a4cca7a23baceda903c76af75
Timur Badretdinov <accounts@destiner.io>
2024-04-26 20:55:10 +0100

    Fetch gas data

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -22,18 +22,24 @@
             <div class="hex">{{ transaction.input }}</div>
           </div>
         </div>
+        <div v-if="actualGasCost && actualGasUsed && actualGasFee">
+          <div>gas used: {{ actualGasUsed }}</div>
+          <div>gas fee: {{ formatGasPrice(actualGasFee) }}</div>
+          <div>cost: {{ formatEther(actualGasCost) }}</div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { Hex, decodeEventLog, formatUnits } from 'viem';
+import { computed, ref } from 'vue';
 
 import useEnv from '@/composables/useEnv';
-import EvmService, { Transaction } from '@/services/evm';
+import EvmService, { Transaction, TransactionReceipt } from '@/services/evm';
 import IndexerService, { TransactionUserOp } from '@/services/indexer';
-import { getChainClient } from '@/utils/chains';
+import { Chain, getChainClient } from '@/utils/chains';
 import { isUserOpHash } from '@/utils/validation/pattern';
 
 const { alchemyApiKey } = useEnv();
@@ -43,6 +49,7 @@ const indexerService = new IndexerService();
 const userOpHash = ref('');
 const userOp = ref<TransactionUserOp | null>(null);
 const transaction = ref<Transaction | null>(null);
+const transactionReceipt = ref<TransactionReceipt | null>(null);
 
 async function handleSubmit(): Promise<void> {
   userOp.value = null;
@@ -57,12 +64,136 @@ async function handleSubmit(): Promise<void> {
     return;
   }
   userOp.value = transactionUserOp;
-  const chain = transactionUserOp.chainId;
+  await Promise.all([
+    fetchTransaction(
+      transactionUserOp.chainId,
+      transactionUserOp.transactionHash,
+    ),
+    fetchTransactionReceipt(
+      transactionUserOp.chainId,
+      transactionUserOp.transactionHash,
+    ),
+  ]);
+}
+
+async function fetchTransaction(chain: Chain, hash: Hex): Promise<void> {
   const client = getChainClient(chain, alchemyApiKey);
   const evmService = new EvmService(chain, client);
-  transaction.value = await evmService.getTransaction(
-    transactionUserOp.transactionHash,
+  transaction.value = await evmService.getTransaction(hash);
+}
+
+async function fetchTransactionReceipt(chain: Chain, hash: Hex): Promise<void> {
+  const client = getChainClient(chain, alchemyApiKey);
+  const evmService = new EvmService(chain, client);
+  transactionReceipt.value = await evmService.getTransactionReceipt(hash);
+}
+
+const userOpEvent = computed(() => {
+  if (!transactionReceipt.value) {
+    return null;
+  }
+  const log = transactionReceipt.value.logs.find(
+    (log) =>
+      log.topics[0] ===
+        '0x49628fd1471006c1482da88028e9ce4dbb080b815c9b0344d39e5a8e6ec1419f' &&
+      log.topics[1] === userOpHash.value,
   );
+  if (!log) {
+    return null;
+  }
+  const event = decodeEventLog({
+    abi: [
+      {
+        anonymous: false,
+        inputs: [
+          {
+            indexed: true,
+            internalType: 'bytes32',
+            name: 'userOpHash',
+            type: 'bytes32',
+          },
+          {
+            indexed: true,
+            internalType: 'address',
+            name: 'sender',
+            type: 'address',
+          },
+          {
+            indexed: true,
+            internalType: 'address',
+            name: 'paymaster',
+            type: 'address',
+          },
+          {
+            indexed: false,
+            internalType: 'uint256',
+            name: 'nonce',
+            type: 'uint256',
+          },
+          {
+            indexed: false,
+            internalType: 'bool',
+            name: 'success',
+            type: 'bool',
+          },
+          {
+            indexed: false,
+            internalType: 'uint256',
+            name: 'actualGasCost',
+            type: 'uint256',
+          },
+          {
+            indexed: false,
+            internalType: 'uint256',
+            name: 'actualGasUsed',
+            type: 'uint256',
+          },
+        ],
+        name: 'UserOperationEvent',
+        type: 'event',
+      },
+    ],
+    data: log.data,
+    topics: log.topics,
+  });
+  return event;
+});
+
+const actualGasUsed = computed<bigint | undefined>(
+  () => userOpEvent.value?.args.actualGasUsed,
+);
+const actualGasCost = computed<bigint | undefined>(
+  () => userOpEvent.value?.args.actualGasCost,
+);
+const actualGasFee = computed<bigint | undefined>(() => {
+  if (!actualGasCost.value || !actualGasUsed.value) {
+    return undefined;
+  }
+  return actualGasCost.value / actualGasUsed.value;
+});
+
+function formatEther(value: bigint): string {
+  return `${formatNumber(fromWei(value, 18))} ETH`;
+}
+
+function formatGasPrice(value: bigint): string {
+  return `${formatNumber(fromWei(value, 9))} Gwei`;
+}
+
+function formatNumber(value: number): string {
+  const valueFormat = new Intl.NumberFormat('en-US', {
+    notation: 'compact',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 8,
+  });
+  return valueFormat.format(value);
+}
+
+function fromWei(value: bigint | number, decimals: number): number {
+  if (typeof value === 'bigint') {
+    return parseFloat(formatUnits(value, decimals));
+  }
+  return parseFloat(formatUnits(BigInt(value.toString()), decimals));
 }
 </script>
 
