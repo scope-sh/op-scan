6658500ecad81406f305b14a9930a6bd68caa891
Timur Badretdinov <accounts@destiner.io>
2024-04-28 00:32:32 +0100

    Add "getLatestUserOps" to indexer service

--- src/services/indexer.ts
+++ src/services/indexer.ts
@@ -60,6 +60,34 @@ interface SenderUserOp {
   nonce: bigint;
 }
 
+interface UserOpFeedResponse {
+  data: {
+    UserOp: {
+      chainId: string;
+      blockNumber: number;
+      blockTimestamp: number;
+      transactionHash: string;
+      sender: string;
+      bundler: string;
+      paymaster: string;
+      entryPoint: string;
+      hash: string;
+    }[];
+  };
+}
+
+interface FeedUserOp {
+  chainId: Chain;
+  blockNumber: number;
+  blockTimestamp: number;
+  transactionHash: Hex;
+  sender: Address;
+  bundler: Address;
+  paymaster: Address;
+  entryPoint: Address;
+  hash: Hex;
+}
+
 class Service {
   endpointUrl: string;
 
@@ -170,7 +198,53 @@ class Service {
       }));
     }
   }
+
+  public async getLatestUserOps(limit: number): Promise<FeedUserOp[]> {
+    const query = `{
+      UserOp(
+        limit: ${limit},
+        order_by: {
+          blockTimestamp: desc
+        }
+      ) {
+        chainId
+        blockNumber
+        blockTimestamp
+        transactionHash
+        sender
+        bundler
+        paymaster
+        entryPoint
+        hash
+      }
+    }`;
+
+    const response = await fetch(this.endpointUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query }),
+    });
+
+    const json = (await response.json()) as UserOpFeedResponse;
+    if (json.data.UserOp.length === 0) {
+      return [];
+    } else {
+      return json.data.UserOp.map((userOp) => ({
+        chainId: parseInt(userOp.chainId) as Chain,
+        blockNumber: userOp.blockNumber,
+        blockTimestamp: userOp.blockTimestamp,
+        transactionHash: userOp.transactionHash as Hex,
+        sender: userOp.sender as Address,
+        bundler: userOp.bundler as Address,
+        paymaster: userOp.paymaster as Address,
+        entryPoint: userOp.entryPoint as Address,
+        hash: userOp.hash as Hex,
+      }));
+    }
+  }
 }
 
 export default Service;
-export type { TransactionUserOp, SenderUserOp };
+export type { TransactionUserOp, SenderUserOp, FeedUserOp };
