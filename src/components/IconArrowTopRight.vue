6d1ab53c4644c514595a1707425eaf1de8436702
Timur Badretdinov <accounts@destiner.io>
2024-04-28 18:07:57 +0100

    Add IconArrowTopRight component

---
+++ src/components/IconArrowTopRight.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg
+    viewBox="0 0 15 15"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill="currentColor"
+      fill-rule="evenodd"
+      d="M3.646 11.354a.5.5 0 0 1 0-.707L10.293 4H6a.5.5 0 0 1 0-1h5.5a.5.5 0 0 1 .5.5V9a.5.5 0 0 1-1 0V4.707l-6.646 6.647a.5.5 0 0 1-.708 0"
+      clip-rule="evenodd"
+    />
+  </svg>
+</template>
