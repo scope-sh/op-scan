79f700339bf8f8cdd1d630846f766918a914200d
Timur Badretdinov <accounts@destiner.io>
2024-04-29 03:00:51 +0100

    Add IconArrowLeft component

---
+++ src/components/IconArrowLeft.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg
+    viewBox="0 0 15 15"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill="currentColor"
+      fill-rule="evenodd"
+      d="M6.854 3.146a.5.5 0 0 1 0 .708L3.707 7H12.5a.5.5 0 0 1 0 1H3.707l3.147 3.146a.5.5 0 0 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 0 1 .708 0"
+      clip-rule="evenodd"
+    />
+  </svg>
+</template>
