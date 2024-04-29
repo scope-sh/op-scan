bafb738486e132d9350c4b0ada19783c95e1e32e
Timur Badretdinov <accounts@destiner.io>
2024-04-29 23:46:28 +0100

    Add 4byte service

---
+++ src/services/4byte.ts
@@ -0,0 +1,30 @@
+import { Hex } from 'viem';
+
+interface HexSignatureResponse {
+  count: number;
+  next: null;
+  previous: null;
+  results: {
+    id: number;
+    created_at: string;
+    text_signature: string;
+    hex_signature: Hex;
+    bytes_signature: string;
+  }[];
+}
+
+class Service {
+  async getSignature(signature: Hex): Promise<string | null> {
+    const response = await fetch(
+      `https://www.4byte.directory/api/v1/signatures/?hex_signature=${signature}`,
+    );
+    const data = (await response.json()) as HexSignatureResponse;
+    if (data.count === 0) {
+      return null;
+    }
+    const firstResult = data.results[0];
+    return firstResult ? firstResult.text_signature : null;
+  }
+}
+
+export default Service;
