2a596672cd0a197e9f8cb4f7a8b7ea20273b3c65
Timur Badretdinov <accounts@destiner.io>
2024-04-29 21:50:13 +0100

    Add native asset formatter

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -1,3 +1,6 @@
+import { formatUnits } from 'viem';
+
+import { Chain, getChainNativeSymbol } from './chains';
 import { RelativeTime } from './conversion';
 
 function formatRelativeTime({ value, unit }: RelativeTime): string {
@@ -5,5 +8,25 @@ function formatRelativeTime({ value, unit }: RelativeTime): string {
   return format.format(value, unit);
 }
 
-// eslint-disable-next-line import/prefer-default-export
-export { formatRelativeTime };
+function formatNative(chain: Chain, value: bigint): string {
+  const nativeSymbol = getChainNativeSymbol(chain);
+  return `${formatNumber(fromWei(value, 18))} ${nativeSymbol}`;
+}
+
+function formatNumber(value: number): string {
+  const valueFormat = new Intl.NumberFormat('en-US', {
+    notation: 'compact',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 8,
+  });
+  return valueFormat.format(value);
+}
+
+function fromWei(value: bigint | number, decimals: number): number {
+  if (typeof value === 'bigint') {
+    return parseFloat(formatUnits(value, decimals));
+  }
+  return parseFloat(formatUnits(BigInt(value.toString()), decimals));
+}
+
+export { formatRelativeTime, formatNative };
