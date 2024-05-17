7ebd975b5d3bfd4b701ceefb52a6b011517a945e
Timur Badretdinov <accounts@destiner.io>
2024-05-17 11:18:16 +0100

    Add IconChevronUp component

---
+++ src/components/IconChevronUp.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg
+    viewBox="0 0 15 15"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill="currentColor"
+      fill-rule="evenodd"
+      d="M3.135 8.842a.5.5 0 0 0 .707.023L7.5 5.435l3.658 3.43a.5.5 0 0 0 .684-.73l-4-3.75a.5.5 0 0 0-.684 0l-4 3.75a.5.5 0 0 0-.023.707"
+      clip-rule="evenodd"
+    />
+  </svg>
+</template>
